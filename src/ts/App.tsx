4bcb95633ce11ef79d462d90aabf20c4e63417d6
Matthias Arnold <arnold.matthias.y@gmail.com>
2023-10-25 08:42:13 +0900

    Got BS icons working

--- src/ts/App.tsx
+++ src/ts/App.tsx
@@ -2,11 +2,17 @@ import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState('name');
 
   return (
     <>
       <div className="container py-4 px-3 mx-auto">
-        <h1>Hello, Bootstrap and Vite!</h1>
+        <h1 className="text-primary text-center">
+          Hello {name}, good to have you here
+        </h1>
+        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>
+          <i className="bi-camera" style={{ fontSize: '4em' }}></i>
+        </button>
         <button
           className="btn btn-primary"
           onClick={() => setCount((count) => count + 1)}
